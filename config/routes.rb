f000de5f6404cbca70525c4444808f9746df1469
MiguelDotL <miguel.d.lozano@gmail.com>
2016-07-16 22:26:45 -0400

    Added User Authorization with bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static#welcome'
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
 
-  get 'static/welcome'
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'signin', to: 'sessions#new', as: 'signin'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :users
+  get 'page/index'
+  root 'page#index'
+
+  # root 'static#welcome'
+  # get 'static/welcome'
 
 end
