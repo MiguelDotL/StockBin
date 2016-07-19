8e793cee24f3e3bf9376e5ab793f551a9f5b66a8
MiguelDotL <miguel.d.lozano@gmail.com>
2016-07-19 03:41:57 -0400

    Home asnd Search Results Pages Styled - Need to work on Signin/Signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  # Authentications BugFix
+  skip_before_filter :verify_authenticity_token, :only => :create
+
   private
     def current_user
       User.where(id: session[:user_id]).first
